cc580c3e66c44be5832542a6374ae0a11ae3d1a2
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-07-31 23:30:45 -0300

    app finalizado

--- DogAge/ViewController.swift
+++ DogAge/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var campoIdadeCachorro: UITextField!
+    @IBOutlet var legendaResultado: UILabel!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        let idade = Int(campoIdadeCachorro.text!)! * 7
+        legendaResultado.text = "A idade do cachorro é: " + String(idade)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
